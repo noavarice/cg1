7724fe1d65cf6610a8aaae1fa28ddfd590b8c51f
noavarice <alex9razinkov@gmail.com>
2017-05-08 16:36:24 +0300

    getCircleCenter now returns single point

--- bresenham.cpp
+++ bresenham.cpp
@@ -6,7 +6,7 @@
  * @brief Находит и возвращает пару точек, являющихся возможными центрами круга, проходящего через
  * p1 и p2
  */
-static QPair<QPoint, QPoint> getCircleCenter(const QPoint& p1, const QPoint& p2, const uint8_t radius)
+static QPoint getCircleCenter(const QPoint& p1, const QPoint& p2, const uint8_t radius)
 {
     int dx, dy;
     if (p1.x() > p2.x()) {
@@ -20,15 +20,15 @@ static QPair<QPoint, QPoint> getCircleCenter(const QPoint& p1, const QPoint& p2,
     int catheterLength = static_cast<int>(qSqrt(radius * radius - lengthSqr / 4));
     QPoint middle = QPoint((p1.x() + p2.x()) / 2, (p1.y() + p2.y()) / 2);
     if (!dx) {
-        return {QPoint(-catheterLength, middle.y()), QPoint(catheterLength, middle.y())};
+        return QPoint(-catheterLength, middle.y());
     }
     if (!dy) {
-        return {QPoint(middle.x(), -catheterLength), QPoint(middle.x(), catheterLength)};
+        return QPoint(middle.x(), -catheterLength);
     }
     qreal coeff = -dx / dy;
     dx = qCos(coeff) * catheterLength;
     dy = qSin(coeff) * catheterLength;
-    return {middle - QPoint(dx, dy), middle + QPoint(dx, dy)};
+    return middle - QPoint(dx, dy);
 }
 
 void bresenhamCircleCurve(INOUT QImage& image,
