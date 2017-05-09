7d799606a67bf852605fe28adca2358032e3762c
noavarice <alex9razinkov@gmail.com>
2017-05-09 15:56:26 +0300

    added slot for draw button click

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "bresenham.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -20,4 +22,11 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButtonReset_clicked()
+{
+    QColor color{ui->spinBoxRed->value(), ui->spinBoxGreen->value(), ui->spinBoxBlue->value()};
+    bresenhamCircleCurve(imageFirst, {100, 100}, {175, 175}, 100, color, ui->spinBoxWidth->value());
+    ui->labelFirstCurve->setPixmap(QPixmap::fromImage(imageFirst));
 }
