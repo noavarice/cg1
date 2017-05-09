bd809907402343ad42b4b24184c00824b2794508
noavarice <alex9razinkov@gmail.com>
2017-05-09 17:27:20 +0300

    added components for setting points' coordinates

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 
 #include "bresenham.h"
 
+#define RADIUS 100
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -27,6 +29,16 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButtonReset_clicked()
 {
     QColor color{ui->spinBoxRed->value(), ui->spinBoxGreen->value(), ui->spinBoxBlue->value()};
-    bresenhamCircleCurve(imageFirst, {100, 100}, {175, 175}, 100, color, ui->spinBoxWidth->value());
+    uint8_t width =  ui->spinBoxWidth->value();
+    QPoint start{ui->spinBoxX1->value(), ui->spinBoxY1->value()};
+    QPoint end{ui->spinBoxX2->value(), ui->spinBoxY2->value()};
+
+    imageFirst.fill(Qt::white);
+    imageSecond.fill(Qt::white);
+
+    bresenhamCircleCurve(imageFirst, start, end, RADIUS, color, width, true);
+    bresenhamCircleCurve(imageSecond, start, end, RADIUS, color, width, false);
+
     ui->labelFirstCurve->setPixmap(QPixmap::fromImage(imageFirst));
+    ui->labelSecondCurve->setPixmap(QPixmap::fromImage(imageSecond));
 }
